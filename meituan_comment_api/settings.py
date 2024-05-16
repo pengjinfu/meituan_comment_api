fc9d6486e38a80cd5fea8296c606b3526059977c
Schawcheng <jyanchou@163.com>
2024-05-16 16:18:05 +0800

    设置静态文件目录

--- meituan_comment_api/settings.py
+++ meituan_comment_api/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 import config.database
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -142,3 +143,4 @@ CORS_ALLOW_METHODS = [
     'PUT',
     'DELETE'
 ]
+
